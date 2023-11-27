dff28464f28fa04ba69d51d046a89d6c5c364cb6
Charlotte Stone <charlie.stone649@gmail.com>
2023-11-27 15:39:08 +0000

    Correct allowed hosts

--- main/settings.py
+++ main/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEVELOPMENT" in os.environ
 
-ALLOWED_HOSTS = ['8000-terafora-hackathon3todo-xqgbzx533r4.ws-eu106.gitpod.io', '.gitpod.io', 'herokuapp.com']
+ALLOWED_HOSTS = ['8000-terafora-hackathon3todo-xqgbzx533r4.ws-eu106.gitpod.io', '.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
