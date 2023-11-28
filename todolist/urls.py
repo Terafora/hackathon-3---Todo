1addfa87ec9afb50e77cb115945694509e1b86f8
Charlotte Stone <charlie.stone649@gmail.com>
2023-11-28 20:06:31 +0000

    Add delete functionality

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Home, CreateNote, NoteDetail
+from .views import Home, CreateNote, NoteDetail, DeleteNote
 
 urlpatterns = [
     path('', Home.as_view(), name="home"),
     path('createnote/', CreateNote.as_view(), name="create_note"),
-    path('>slug:pk>/', NoteDetail.as_view(), name="note_detail"),
+    path('<slug:pk>/', NoteDetail.as_view(), name="note_detail"),
+    path('<slug:pk>/', NoteDelete.as_view(), name="note_delete"),
 ]
