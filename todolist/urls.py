51b337bf6760b0dfa255eb7501d1ee1223bb29c3
Charlotte Stone <charlie.stone649@gmail.com>
2023-11-28 16:11:20 +0000

    Update forms, models and views

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Home
+from .views import Home, CreateNote
 
 urlpatterns = [
     path('', Home.as_view(), name="home"),
+    path('', CreateNote.as_view(), name="create_note"),
 ]
