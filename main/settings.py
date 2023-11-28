530ba120cf031ce50dd6b190b85da95a4350e628
Charlotte Stone <charlie.stone649@gmail.com>
2023-11-28 11:58:34 +0000

    Further update to settings

--- main/settings.py
+++ main/settings.py
@@ -190,11 +190,12 @@ LOGIN_REDIRECT_URL = '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MIME_TYPES = {
     'text/html': '.css',
